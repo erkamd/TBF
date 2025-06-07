acc1d861a206914d02a270cb9f44ceb36373783a
wArtist <erkmdtgl0@hotmail.com>
2025-06-07 15:14:50 +0300

    Revert "Merge remote-tracking branch 'origin/main'"
    This reverts commit 2e6c7da52aa28e0389cb8d4aaf0d607a3a2b45d8, reversing
    changes made to 8c69f7f5344f84c46f20de99a37ba97b27914a31.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,21 +3,12 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance { get; private set; }
     public GameObject agentPrefab;
 
     private readonly List<AgentController> teamA = new();
     private readonly List<AgentController> teamB = new();
     private readonly List<AgentController> turnOrder = new();
     private int currentTurn;
-
-    private readonly Dictionary<Vector2Int, AgentController> board = new();
-    public BallController ball;
-
-    private void Awake()
-    {
-        Instance = this;
-    }
 
     private void Start()
     {
@@ -27,7 +18,6 @@ public class GameManager : MonoBehaviour
         }
 
         SpawnTeams();
-        SpawnBall();
         DetermineTurnOrder();
     }
 
@@ -39,27 +29,15 @@ public class GameManager : MonoBehaviour
             var a = Instantiate(agentPrefab);
             var ac = a.GetComponent<AgentController>();
             ac.Initialize(new Vector2Int(centerRow - 1 + i, 3));
-            ac.number = i + 1;
             teamA.Add(ac);
-            board[ac.gridPosition] = ac;
 
             var b = Instantiate(agentPrefab);
             var bc = b.GetComponent<AgentController>();
             bc.Initialize(new Vector2Int(centerRow - 1 + i, GridManager.Instance.columns - 4));
-            bc.number = i + 4;
             teamB.Add(bc);
-            board[bc.gridPosition] = bc;
         }
 
         teamA[0].hasBall = true;
-    }
-
-    private void SpawnBall()
-    {
-        var go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        ball = go.AddComponent<BallController>();
-        ball.possessor = teamA[0];
-        ball.transform.position = teamA[0].transform.position;
     }
 
     private void DetermineTurnOrder()
@@ -67,13 +45,7 @@ public class GameManager : MonoBehaviour
         turnOrder.Clear();
         turnOrder.AddRange(teamA);
         turnOrder.AddRange(teamB);
-        for (int i = 0; i < turnOrder.Count; i++)
-        {
-            int j = Random.Range(i, turnOrder.Count);
-            var temp = turnOrder[i];
-            turnOrder[i] = turnOrder[j];
-            turnOrder[j] = temp;
-        }
+        turnOrder.Sort((a, b) => (b.stats.awareness + Dice.Roll(20)).CompareTo(a.stats.awareness + Dice.Roll(20)));
         currentTurn = 0;
     }
 
@@ -86,40 +58,6 @@ public class GameManager : MonoBehaviour
 
     public void EndTurn()
     {
-        if (ball != null)
-        {
-            ball.MoveBall();
-        }
-
         currentTurn++;
-
-        if (currentTurn >= turnOrder.Count)
-        {
-            DetermineTurnOrder();
-        }
-    }
-
-    public bool IsCellOccupied(Vector2Int cell)
-    {
-        return board.ContainsKey(cell);
-    }
-
-    public void UpdateAgentCell(AgentController agent, Vector2Int newCell)
-    {
-        board.Remove(agent.gridPosition);
-        board[newCell] = agent;
-    }
-
-    private void OnGUI()
-    {
-        GUILayout.BeginHorizontal();
-        foreach (var agent in turnOrder)
-        {
-            string label = agent.number.ToString();
-            if (agent == GetCurrentAgent())
-                label = $">{label}<";
-            GUILayout.Label(label, GUILayout.Width(30));
-        }
-        GUILayout.EndHorizontal();
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Turn Based Football (TBF)
 
-This project contains a minimal prototype of a turn based football game built with Unity. The game uses a 15x40 grid and spawns two teams of three agents. Each agent has several attributes such as Passing, Shooting and Awareness. A simple dice system is used for actions. Agents are assigned jersey numbers and turns are taken one agent at a time in a randomly generated order every cycle.
+This project contains a minimal prototype of a turn based football game built with Unity. The game uses a 15x40 grid and spawns two teams of three agents. Each agent has several attributes such as Passing, Shooting and Awareness. A simple dice system is used for actions.
 
 ## How to run
 
@@ -9,4 +9,3 @@ This project contains a minimal prototype of a turn based football game built wi
 3. Press Play to generate the grid and spawn the placeholder agents.
 
 Agents and grid visuals are simple primitives. GameManager automatically loads the agent prefab from `Resources/Prefabs/Agent`.
-At the end of each turn the ball moves using its current velocity if it is not possessed by any agent.
